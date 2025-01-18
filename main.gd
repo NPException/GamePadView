a905af7cfc697b9fa3ead302864584375a98bdf1
NPException <npexception42@gmail.com>
2025-01-18 17:39:45 +0100

    add konami code easter egg

--- main.gd
+++ main.gd
@@ -14,48 +14,83 @@ func _unhandled_input(event: InputEvent) -> void:
 	elif event is InputEventJoypadMotion:
 		motion_event(event)
 
+# Up, Up, Down, Down, Left, Right, Left, Right, B, A, Start
+var konami : Array[JoyButton] = [
+	JOY_BUTTON_DPAD_UP,
+	JOY_BUTTON_DPAD_UP,
+	JOY_BUTTON_DPAD_DOWN,
+	JOY_BUTTON_DPAD_DOWN,
+	JOY_BUTTON_DPAD_LEFT,
+	JOY_BUTTON_DPAD_RIGHT,
+	JOY_BUTTON_DPAD_LEFT,
+	JOY_BUTTON_DPAD_RIGHT,
+	JOY_BUTTON_B,
+	JOY_BUTTON_A,
+	JOY_BUTTON_START
+]
+
+var matchIndex := 0
+
+func checkKonami(button : JoyButton) -> void:
+	if button != konami[matchIndex]:
+		matchIndex = 0
+	else:
+		print(button)
+		matchIndex += 1
+		# check if done
+		if matchIndex == konami.size():
+			matchIndex = 0
+			%HugeFraudImage.visible = true
+			await get_tree().create_timer(3.0).timeout # waits for 1 second
+			%HugeFraudImage.visible = false
+
+
 
 func button_event(event: InputEventJoypadButton) -> void:
 	var on := event.pressed
 	var off := !on
+	
+	if off:
+		checkKonami(event.button_index)
+	
 	match event.button_index:
-		JoyButton.JOY_BUTTON_A:
+		JOY_BUTTON_A:
 			%Button_A/Off.visible = off
 			%Button_A/On.visible = on
-		JoyButton.JOY_BUTTON_B:
+		JOY_BUTTON_B:
 			%Button_B/Off.visible = off
 			%Button_B/On.visible = on
-		JoyButton.JOY_BUTTON_X:
+		JOY_BUTTON_X:
 			%Button_X/Off.visible = off
 			%Button_X/On.visible = on
-		JoyButton.JOY_BUTTON_Y:
+		JOY_BUTTON_Y:
 			%Button_Y/Off.visible = off
 			%Button_Y/On.visible = on
-		JoyButton.JOY_BUTTON_START:
+		JOY_BUTTON_START:
 			%Button_Menu/Off.visible = off
 			%Button_Menu/On.visible = on
-		JoyButton.JOY_BUTTON_BACK:
+		JOY_BUTTON_BACK:
 			%Button_View/Off.visible = off
 			%Button_View/On.visible = on
-		JoyButton.JOY_BUTTON_LEFT_SHOULDER:
+		JOY_BUTTON_LEFT_SHOULDER:
 			%Button_LB/Off.visible = off
 			%Button_LB/On.visible = on
-		JoyButton.JOY_BUTTON_RIGHT_SHOULDER:
+		JOY_BUTTON_RIGHT_SHOULDER:
 			%Button_RB/Off.visible = off
 			%Button_RB/On.visible = on
-		JoyButton.JOY_BUTTON_LEFT_STICK:
+		JOY_BUTTON_LEFT_STICK:
 			%LeftStick/Off.visible = off
 			%LeftStick/On.visible = on
-		JoyButton.JOY_BUTTON_RIGHT_STICK:
+		JOY_BUTTON_RIGHT_STICK:
 			%RightStick/Off.visible = off
 			%RightStick/On.visible = on
-		JoyButton.JOY_BUTTON_DPAD_UP:
+		JOY_BUTTON_DPAD_UP:
 			%Button_DPAD/Up.visible = on
-		JoyButton.JOY_BUTTON_DPAD_DOWN:
+		JOY_BUTTON_DPAD_DOWN:
 			%Button_DPAD/Down.visible = on
-		JoyButton.JOY_BUTTON_DPAD_LEFT:
+		JOY_BUTTON_DPAD_LEFT:
 			%Button_DPAD/Left.visible = on
-		JoyButton.JOY_BUTTON_DPAD_RIGHT:
+		JOY_BUTTON_DPAD_RIGHT:
 			%Button_DPAD/Right.visible = on
 
 
